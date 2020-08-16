646faa67c8040541f3c39af76eb4627393427b71
Michael Diggin <michael.diggin@gmail.com>
2020-08-16 16:17:04 +0100

    cleaned up code for simple comparision

--- main.cpp
+++ main.cpp
@@ -10,30 +10,14 @@
 using namespace std;
 using namespace cv;
 
-int iround(double d)
-{
-  return static_cast<int>(floor(d + 0.5));
-}
-
-Point rotatePointAbout(Point pt, double x, double y, double theta) {
-    Point res = Point(0, 0);
-    double ptx, pty;
-    ptx = pt.x - x;
-    pty = pt.y - y;
-
-    res.x = iround( ptx * cos(theta) - pty * sin(theta) + x );
-    res.y = iround( ptx * sin(theta) + pty * cos(theta) + y );
-
-    return res;
-}
 
 int main() {
 
-    Moments m1, m2, m3;
-    double t1, t2, t3;
-    Mat image, rotated;
+    Moments m1, m2;
+    double t1, t2;
+    Mat image;
 
-	image = imread("images\\brain.jpg", IMREAD_GRAYSCALE);
+	image = imread("images\\750x750.jpg", IMREAD_GRAYSCALE);
 	if (!image.data)
     {
         cout << "Could not find image" << endl;
@@ -48,81 +32,20 @@ int main() {
 
     pre_compute_power_arrays(image.size());
 
-    int w0 = image.size().width;
-    int h0 = image.size().height;
-    int w2 = w0/2;
-    int h2 = h0/2;
-    int w4 = w0/4;
-    int h4 = h0/4;
 
-    Mat display;
-    Mat v, h, a, d;
-    int r;
+    t1 = (double)getTickCount();
+    for(int i = 0; i < 100; i++)
+        m1 = drt_moments(image);
+    t1 = ((double)getTickCount()-t1)/getTickFrequency();
 
-    for(r = 0; r < 180; r++) {
-        Mat rot = getRotationMatrix2D(Point(w2, h2), r, 1 - static_cast<double>(r) / 400.0);
-        Size sz = Size(image.size());
-
-        warpAffine(image, rotated, rot, sz);
-        
-        cvtColor(rotated, display, COLOR_GRAY2RGB);
-
-        t3 = (double)getTickCount();
-        for(int i = 0; i < 10; i++)
-            m3 = naive_moments(rotated);
-        t3 = ((double)getTickCount()-t3)*10.0/getTickFrequency();
-
-        t2 = (double)getTickCount();
-        for(int i = 0; i < 100; i++)
-            m2 = opencv_moments(rotated);
-        t2 = ((double)getTickCount()-t2)/getTickFrequency();
-
-        t1 = (double)getTickCount();
-        for(int i = 0; i < 100; i++)
-            m1 = drt_moments(rotated);
-        t1 = ((double)getTickCount()-t1)/getTickFrequency();
-        
-        double x = m1.m10 / m1.m00;
-        double y = m1.m01 / m1.m00;
-
-        double u20 = m1.m20/m1.m00 - x*x;
-        double u02 = m1.m02/m1.m00 - y*y;
-        double u11 = m1.m11/m1.m00 - x*y;
-        double theta = 0.5*atan(2*u11/(u20-u02));
-
-        Point startx = rotatePointAbout(Point(x-w4, y), x, y, theta);
-        Point endx = rotatePointAbout(Point(x+w4, y), x, y, theta);
-
-        Point starty = rotatePointAbout(Point(x, y-h4), x, y, theta);
-        Point endy = rotatePointAbout(Point(x, y+h4), x, y, theta);
-
-        line(display, startx, endx, Scalar(255, 0, 0), 2);
-        line(display, starty, endy, Scalar(255, 0, 0), 2);
-
-        drt_images(rotated, v, h, d, a);
-
-        cv::namedWindow("Image");
-        cv::imshow("Image", display);
-
-        cv::namedWindow("v");
-        cv::imshow("v", v);
-
-        cv::namedWindow("h");
-        cv::imshow("h", h);
-
-        cv::namedWindow("d");
-        cv::imshow("d", d);
-
-        cv::namedWindow("a");
-        cv::imshow("a", a);
-
-        waitKey(1);
-    }
+    t2 = (double)getTickCount();
+    for(int i = 0; i < 100; i++)
+        m2 = opencv_moments(image);
+    t2 = ((double)getTickCount()-t2)/getTickFrequency();
 
 
-    cout << "DRT Total: " << t1 << endl;
-    cout << "OCV Total: " << t2 << endl;
-    cout << "MOM Total: " << t3 << endl;
+    cout << "DRT Total: " << t1/1000 << endl;
+    cout << "OCV Total: " << t2/1000 << endl;
 
     cout << m1.m00 << "] m00 [" << m2.m00 << endl;
     cout << m1.m10 << "] m10 [" << m2.m10 << endl;
--- moments.cpp
+++ moments.cpp
@@ -43,92 +43,6 @@ void pre_compute_power_arrays(const Size s) {
     }
 }
 
-void drt_images(const Mat& image, 
-        Mat& v,
-        Mat& h,
-        Mat& d,
-        Mat& a)
-{
-
-    Size s = image.size();
-    const int width = s.width;
-    const int height = s.height;
-
-    //projection arrays
-    vector<long> vert(width, 0);
-    vector<long> hor(height, 0);
-    vector<long> diag(width+height, 0);
-    vector<long> anti(width+height, 0);
-
-    long* hptr = &hor[0];
-    long* initvptr = &vert[0];
-    long* vptr;
-    long* dptr;
-    long* aptr;
-    vptr = initvptr;
-
-    // loop through image by pixel values
-    for (int i=0; i< height; ++i)
-    {
-        dptr = &diag[i];
-        aptr = &anti[height-1-i];
-        const uchar* p = image.ptr<uchar>(i);
-
-        for(int j = 0; j < width; ++j)
-        {
-            vptr[j] += p[j];
-            hptr[i] += p[j];
-            dptr[j] += p[j];
-            aptr[j] += p[j];
-        }
-
-    }
-
-    int pos = std::distance(hor.begin(), std::max_element(hor.begin(), hor.end()));
-    h = Mat(height, static_cast<int>(hor[pos]/255*1.5), image.type());
-    h = 0;
-
-    for(int x = 0; x < height; x++)
-    {
-        uchar *p = h.ptr<uchar>(x);
-        for(int y = 0; y < static_cast<int>(hor[x]/255.0); y++)
-        {
-            p[y] = 255;
-        }
-    }
-
-    pos = std::distance(vert.begin(), std::max_element(vert.begin(), vert.end()));
-    v = Mat(static_cast<int>(vert[pos]/255*1.5), width, image.type());
-    v = 0;
-
-    for(int y = 0; y < width; y++)
-    {
-        for(int x = 0; x < static_cast<int>(vert[y]/255.0); x++)
-        {
-            uchar *p = v.ptr<uchar>(x, y);
-            *p = 255;
-        }
-    }
-
-    pos = std::distance(diag.begin(), std::max_element(diag.begin(), diag.end()));
-    d = Mat(width + height, static_cast<int>(diag[pos]/255*1.5), image.type());
-    d = 0;
-
-    pos = std::distance(anti.begin(), std::max_element(anti.begin(), anti.end()));
-    a = Mat(width + height, static_cast<int>(anti[pos]/255*1.5), image.type());
-    a = 0;
-
-    for(int x = 0; x < width + height; x++) {
-        for(int y = 0; y < static_cast<int>(diag[x]/255.0); y++) {
-            uchar *p = d.ptr<uchar>(x, y);
-            *p = 255;
-        }
-        for(int y = 0; y < static_cast<int>(anti[x]/255.0); y++) {
-            uchar *p = a.ptr<uchar>(x, y);
-            *p = 255;
-        }
-    }
-}
 
 Moments drt_moments(const Mat& image)
 {
@@ -183,7 +97,8 @@ Moments drt_moments(const Mat& image)
     return m;
 }
 
-Moments opencv_moments_double(const Mat& image)
+template <typename T> 
+Moments opencv_moments_template(const Mat& image)
 {
     Size s = image.size();
 
@@ -193,11 +108,11 @@ Moments opencv_moments_double(const Mat& image)
     for(int y = 0; y < s.height; y++ )
     {
         const uchar* p = image.ptr<uchar>(y);
-        long x0 = 0;
-        double x1 = 0.0, x2 = 0.0, x3 = 0.0;
+        T x0 = 0;
+        T x1 = 0.0, x2 = 0.0, x3 = 0.0;
 
         for(int x = 0; x < s.width; x++ ) {
-            double xp = x * p[x], xxp = x * xp;
+            T xp = x * p[x], xxp = x * xp;
 
             x0 += p[x];
             x1 += xp;
@@ -205,46 +120,7 @@ Moments opencv_moments_double(const Mat& image)
             x3 += xxp * x;
         }
 
-        double py = y * x0, sy = y*y;
-
-        m03 += py * sy;  
-        m12 += x1 * sy;  
-        m21 += x2 * y;  
-        m30 += x3;
-        m02 += x0 * sy;
-        m11 += x1 * y;
-        m20 += x2;
-        m01 += py;
-        m10 += x1;
-        m00 += x0;
-    }
-
-    Moments m(m00, m10, m01, m20, m11, m02, m30, m21, m12, m03);
-    return m;
-}
-
-Moments opencv_moments_long(const Mat& image)
-{
-    Size s = image.size();
-
-    double m00 = 0.0, m01 = 0.0, m10 = 0.0, m11 = 0.0, m20 = 0.0, m02 = 0.0;
-    double m30 = 0.0, m12 = 0.0, m21 = 0.0, m03 = 0.0;
-
-    for(int y = 0; y < s.height; y++ ) {
-        const uchar* p = image.ptr<uchar>(y);
-        unsigned long x0 = 0;
-        unsigned long x1 = 0.0, x2 = 0.0, x3 = 0.0;
-
-        for(int x = 0; x < s.width; x++ ) {
-            unsigned long xp = x * p[x], xxp = x * xp;
-
-            x0 += p[x];
-            x1 += xp;
-            x2 += xxp;
-            x3 += xxp * x;
-        }
-
-        unsigned long py = y * x0, sy = y*y;
+        T py = y * x0, sy = y*y;
 
         m03 += py * sy;  
         m12 += x1 * sy;  
@@ -264,10 +140,10 @@ Moments opencv_moments_long(const Mat& image)
 
 Moments opencv_moments(const Mat& image) {
     Size s = image.size();
-    if(s.height > 250 || s.width > 2500)
-        return opencv_moments_double(image);
+    if(s.height > 250 || s.width > 250)
+        return opencv_moments_template<double>(image);
     else
-        return opencv_moments_long(image);
+        return opencv_moments_template<unsigned long>(image);
 }
 
 Moments naive_moments(const Mat& image)
